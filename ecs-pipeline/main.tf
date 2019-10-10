a7030c812f0bcffca473ffd1b44f0273828bda7a
Morgan <morgan@thirteen.local>
2019-10-09 20:38:30 -0500

    ECS Pipeline Module

---
+++ ecs-pipeline/main.tf
@@ -0,0 +1,265 @@
+provider "aws" {
+  region = "${var.aws_region}"
+  profile = "${var.aws_profile}"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "${var.remote_state_bucket}"
+    key = "${var.remote_state_key}"
+    region = "${var.remote_state_region}"
+  }
+}
+
+/* remote states */
+
+data "terraform_remote_state" "codebuild_bucket" {
+  backend = "s3"
+  config = {
+    bucket = "${var.remote_state_bucket}"
+    key = "${var.s3_codebuild_remote_state_bucket_key}"
+    region = "${var.aws_region}"
+  }
+}
+
+data "terraform_remote_state" "roles" {
+  backend = "s3"
+  config = {
+    bucket = "${var.remote_state_bucket}"
+    key = "${var.iam_remote_state_bucket_key}"
+    region = "${var.aws_region}"
+  }
+}
+
+data "terraform_remote_state" "cluster" {
+  backend = "s3"
+  config = {
+    bucket = "${var.remote_state_bucket}"
+    key = "${var.cluster_remote_state_bucket_key}"
+    region = "${var.aws_region}"
+  }
+}
+
+data "terraform_remote_state" "codepipeline_bucket" {
+  backend = "s3"
+  config = {
+    bucket = "${var.remote_state_bucket}"
+    key = "${var.s3_codepipeline_remote_state_bucket_key}"
+    region = "${var.aws_region}"
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "${var.remote_state_bucket}"
+    key = "${var.vpc_remote_state_bucket_key}"
+    region = "${var.aws_region}"
+  }
+}
+
+data "terraform_remote_state" "security_group" {
+  backend = "s3"
+  config = {
+    bucket = "${var.remote_state_bucket}"
+    key = "${var.sg_remote_state_bucket_key}"
+    region = "${var.aws_region}"
+  }
+}
+
+/* CODEBUILD */
+/* specific codebuild "folder" */
+
+resource "aws_s3_bucket_object" "codebuild_bucket" {
+    bucket  = "${data.terraform_remote_state.codebuild_bucket.bucket_id}"
+    key     = "${var.name}/"
+    source  = "/dev/null"
+}
+
+/* ecr repo */
+resource "aws_ecr_repository" "ecr_repo" {
+    name = "${var.name}"
+}
+
+/* codebuild project */
+// CODEPIPELINE SOURCE
+resource "aws_codebuild_project" "project" {
+    name          = "${var.name}-build"
+    description   = "${var.name} Codebuild project"
+    build_timeout = "${var.build_timeout}"
+    service_role  = "${data.terraform_remote_state.roles.codebuild_role_arn}"
+
+    artifacts {
+      type = "${var.artifact_type}"
+    }
+
+    environment {
+      compute_type    = "${var.compute_type}"
+      image           = "${var.image_type}"
+      type            = "${var.env_type}"
+      privileged_mode = true
+
+      environment_variable {
+        "name"  = "${var.env_var_region_name}"
+        "value" = "${var.aws_region}"
+      }
+
+      environment_variable {
+        "name"  = "${var.env_var_repo_url}"
+        "value" = "${aws_ecr_repository.ecr_repo.repository_url}"
+      }
+
+      environment_variable {
+        "name"  = "${var.env_var_name}"
+        "value" = "${var.name}"
+      }
+    }
+
+    source {
+      type  = "${var.source_type}"
+    }
+}
+
+/* ECS SERVICE */
+
+resource "aws_cloudwatch_log_group" "ecs_service_logs" {
+    name = "/${var.prefix}/${var.name}"
+    retention_in_days = "${var.log_retention}"
+}
+
+data "template_file" "task_json" {
+  template = "${file("${path.module}/tasks/${var.task_json_file}")}"
+
+  vars {
+    name      = "${var.name}"
+    port      = "${var.port}"
+    network   = "${var.network_mode}"
+    image     = "${aws_ecr_repository.ecr_repo.repository_url}"
+    memory    = "${var.memory}"
+    log_group = "/${var.prefix}/${var.name}"
+    region    = "${var.aws_region}"
+  }
+}
+
+resource "aws_ecs_task_definition" "task" {
+    family                    = "${var.name}"
+    container_definitions     = "${data.template_file.task_json.rendered}"
+    requires_compatibilities  = ["${var.compatibilities}"]
+    network_mode              = "${var.network_mode}"
+    cpu                       = "${var.cpu}"
+    memory                    = "${var.memory}"
+    execution_role_arn        = "${data.terraform_remote_state.roles.ecs_execution_role_arn}"
+    task_role_arn             = "${data.terraform_remote_state.roles.ecs_execution_role_arn}"
+}
+
+data "aws_ecs_task_definition" "task" {
+  task_definition = "${aws_ecs_task_definition.task.family}"
+  depends_on      = ["aws_ecs_task_definition.task"]
+}
+
+resource "aws_ecs_service" "ecs_service" {
+    name            = "${var.name}"
+    cluster         = "${data.terraform_remote_state.cluster.cluster_id}"
+    task_definition = "${aws_ecs_task_definition.task.family}:${max("${aws_ecs_task_definition.task.revision}", "${data.aws_ecs_task_definition.task.revision}")}"
+    desired_count   = "${var.desired_count}"
+    launch_type     = "${var.launch_type}"
+
+    lifecycle {
+      ignore_changes = ["desired_count"]
+    }
+
+    network_configuration {
+      security_groups = ["${data.terraform_remote_state.security_group.security_group_id}"]
+      subnets         = ["${data.terraform_remote_state.vpc.subnet_ids}"]
+      assign_public_ip = "${var.public_ip}"
+    }
+}
+
+/* CODEPIPELINE */
+resource "aws_codepipeline" "pipeline" {
+  name      = "${var.name}"
+  role_arn  = "${data.terraform_remote_state.roles.codepipeline_role_arn}"
+
+  artifact_store {
+    location  = "${data.terraform_remote_state.codepipeline_bucket.bucket_id}"
+    type = "S3"
+    }
+
+  stage {
+    name = "Source"
+
+    action {
+      name = "Source"
+      category = "Source"
+      owner   = "AWS"
+      provider = "S3"
+      version = "1"
+      output_artifacts = ["source"]
+
+      configuration {
+        S3Bucket = "${data.terraform_remote_state.codebuild_bucket.bucket_id}/${var.name}"
+        S3ObjectKey = "${var.name}.zip"
+        PollForSourceChanges = "false"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name = "Build"
+      category = "Build"
+      owner   = "AWS"
+      provider = "CodeBuild"
+      version = "1"
+      input_artifacts   = ["source"]
+      output_artifacts = ["imagedefinitions"]
+
+      configuration {
+        ProjectName = "${var.name}-build"
+      }
+    }
+  }
+
+  stage {
+    name = "Production"
+
+    action {
+      name = "Deploy"
+      category = "Deploy"
+      owner   = "AWS"
+      provider = "ECS"
+      version = "1"
+      input_artifacts   = ["imagedefinitions"]
+
+      configuration {
+        ClusterName = "${data.terraform_remote_state.cluster.cluster_id}"
+        ServiceName = "${aws_ecs_service.ecs_service.name}"
+        FileName = "imagedefinitions.json"
+      }
+    }
+  }
+}
+
+data "template_file" "event_rule_json" {
+  template = "${file("${path.module}/event_rule.json")}"
+
+  vars {
+    codebuild_bucket = "${data.terraform_remote_state.codebuild_bucket.bucket_id}"
+    name      = "${var.name}"
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "event_codepipeline" {
+    name = "isfs.sys.codepipeline.${var.name}"
+    description = "Triggers ${var.name} codepipeline to build from S3 changes."
+    event_pattern = "${data.template_file.event_rule_json.rendered}"
+
+}
+
+resource "aws_cloudwatch_event_target" "codepipeline_target" {
+    rule      = "${aws_cloudwatch_event_rule.event_codepipeline.name}"
+    arn       = "${aws_codepipeline.pipeline.arn}"
+    role_arn  = "${data.terraform_remote_state.roles.cloudwatch_events_codepipeline_role_arn}"
+}
---
+++ ecs-pipeline/variables.tf
@@ -0,0 +1,150 @@
+variable "aws_profile" {
+  description = "Terraform profile"
+}
+
+variable "aws_region" {
+  description = "us-east-2"
+}
+
+variable "remote_state_key" {
+  description = "State file path"
+}
+
+variable "dynamodb_table" {
+  default = "DynamoDB table for state locking"
+}
+
+/* remote states */
+variable "remote_state_bucket" {
+  description = "S3 Remote State bucket"
+}
+
+variable "iam_remote_state_bucket_key" {
+  description = "Remote state bucket key for IAM state file"
+}
+
+variable "sg_remote_state_bucket_key" {
+  description = "Remote state bucket key for Security Group state file"
+}
+
+variable "vpc_remote_state_bucket_key" {
+  description = "Remote state bucket key for VPC state file"
+}
+
+variable "s3_codepipeline_remote_state_bucket_key" {
+  description = "Remote state bucket key for S3 codepipeline bucket"
+}
+
+variable "s3_codebuild_remote_state_bucket_key" {
+  description = "Remote state bucket key for s3 codebuild bucket state file"
+}
+
+variable "cluster_remote_state_bucket_key" {
+  description = "Remote state bucket key for Cluster state file"
+}
+
+/* codebuild build environment variables */
+variable "name" {
+  description = "Process Name"
+}
+
+variable "build_timeout" {
+  description = "Codebuild timeout in minutes"
+  default = "60"
+}
+
+variable "artifact_type" {
+  description = "Codebuild or S3"
+  default = "CODEPIPELINE"
+}
+
+variable "compute_type" {
+  description = "Codebuild instance building size"
+  default = "BUILD_GENERAL1_SMALL"
+}
+
+variable "image_type" {
+  description = "AWS image type, eg. Docker, Java"
+  default = "aws/codebuild/docker:17.09.0"
+}
+
+variable "env_type" {
+  description = "LINUX_CONTAINER"
+  default = "LINUX_CONTAINER"
+}
+
+variable "env_var_region_name" {
+  description = "AWS_DEFAULT_REGION"
+  default = "AWS_DEFAULT_REGION"
+}
+
+variable "env_var_repo_url" {
+  description = "REPOSITORY_URI"
+  default = "REPOSITORY_URI"
+}
+
+variable "env_var_name" {
+  description = "Container name for image_definitions.json and ecr prefix tagging"
+  default = "NAME"
+}
+
+variable "source_type" {
+  description = "Codepipeline or S3"
+  default = "CODEPIPELINE"
+}
+
+/* ecs variables */
+// cloudwatch variables
+variable "prefix" {
+  description = "Prefix, typically company name related"
+}
+
+variable "log_retention" {
+  default = "14"
+}
+
+// task json variables
+
+variable "task_json_file" {
+  description = "Container task defintion file"
+}
+
+variable "port" {
+  description = "ECS task port"
+}
+
+variable "memory" {
+  description = "Memory size in MB"
+  default = "512"
+}
+
+// task defintion variables
+
+variable "compatibilities" {
+  description = "Needed for Fargate"
+  default = "FARGATE"
+}
+
+variable "network_mode" {
+  description = "Fargate requires 'awsvpc', EC2 can use 'awsvpc or 'bridged'"
+  default = "awsvpc"
+}
+
+variable "cpu" {
+  description = "Size in MB"
+  default = "256"
+}
+
+variable "desired_count" {
+  description = "Task count"
+}
+
+variable "launch_type" {
+  description = "Fargate / EC2"
+  default = "FARGATE"
+}
+
+variable "public_ip" {
+  description = "Auto-assign public IP (true/false)"
+  default = "false"
+}
