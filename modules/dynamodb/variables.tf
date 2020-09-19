825f8c33e1c474917a3a270f1668889090c06ccf
Morgan Tho <morgantche@gmail.com>
2020-09-19 00:24:24 -0500

    add default variables

--- modules/dynamodb/variables.tf
+++ modules/dynamodb/variables.tf
@@ -1,20 +1,25 @@
 variable "aws_region" {
+  default = "us-east-2"
   }
 
 variable "aws_profile" {
+  default = "default"
   }
 
 variable "remote_state_bucket" {
+  default = "morgantho-terraform-remote-state"
   }
 
 variable "remote_state_key" {
   }
 
 variable "remote_state_region" {
+  default = "us-east-2"
   }
 
 variable "dynamodb_table" {
-  } 
+  default = "terraform-state"
+  }
 
 variable "table_name" {
   }
